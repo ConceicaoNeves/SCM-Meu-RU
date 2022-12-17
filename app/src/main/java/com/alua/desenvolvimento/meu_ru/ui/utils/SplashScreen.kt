9035399a94de2e506920375a9458f2d5e6b92c39
Felipe V Assunção <felippevassuncao@gmail.com>
2022-12-17 15:11:31 -0400

    [SplashScreen] Tela de Apresentação

---
+++ app/src/main/java/com/alua/desenvolvimento/meu_ru/ui/utils/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.alua.desenvolvimento.meu_ru.ui.utils
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.alua.desenvolvimento.meu_ru.R
+import com.alua.desenvolvimento.meu_ru.ui.auth.LoginActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
