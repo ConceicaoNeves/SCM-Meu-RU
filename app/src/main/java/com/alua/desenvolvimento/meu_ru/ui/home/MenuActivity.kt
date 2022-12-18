d18016daac2995f635716c4ec8e2bd1b01bcce80
Felipe V Assunção <felippevassuncao@gmail.com>
2022-12-17 23:00:38 -0400

    [LOGIN] Criacao tela Login

---
+++ app/src/main/java/com/alua/desenvolvimento/meu_ru/ui/home/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.alua.desenvolvimento.meu_ru.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alua.desenvolvimento.meu_ru.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
