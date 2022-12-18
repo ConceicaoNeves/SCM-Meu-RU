dd027bba20eb7fd379f962f5961e3c98eeffd55a
Felipe V Assunção <felippevassuncao@gmail.com>
2022-12-18 10:49:09 -0400

    :package: [MENU] Criacao tela home do aplicativo

---
+++ app/src/main/java/com/alua/desenvolvimento/meu_ru/services/cardapios/DiasCardapioActivity.kt
@@ -0,0 +1,12 @@
+package com.alua.desenvolvimento.meu_ru.services.cardapios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alua.desenvolvimento.meu_ru.R
+
+class DiasCardapioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dias_cardapio)
+    }
+}
---
+++ app/src/main/java/com/alua/desenvolvimento/meu_ru/ui/utils/DeclaracaoQRcode.kt
@@ -0,0 +1,12 @@
+package com.alua.desenvolvimento.meu_ru.ui.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alua.desenvolvimento.meu_ru.R
+
+class DeclaracaoQRcode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_declaracao_qrcode)
+    }
+}
